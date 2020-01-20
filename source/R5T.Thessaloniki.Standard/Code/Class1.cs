75ade696c4d700c538c0db48c414522be5f33620
MinexAutomation <minex.automation@gmail.com>
2020-01-19 18:12:37 -0800

    Added AddCTempTemporaryDirectoryPathProvider() IServiceCollection extension.

--- source/R5T.Thessaloniki.Standard/Code/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace R5T.Thessaloniki.Standard
-{
-    public class Class1
-    {
-    }
-}
---
+++ source/R5T.Thessaloniki.Standard/Code/Services/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.Thessaloniki.Default;
+
+
+namespace R5T.Thessaloniki.Standard
+{
+    public static class IServiceCollectionExtensions
+    {
+        public static IServiceCollection AddCTempTemporaryDirectoryPathProvider(this IServiceCollection services)
+        {
+            services.AddSingleton<ITemporaryDirectoryPathProvider, CTempTemporaryDirectoryPathProvider>();
+
+            return services;
+        }
+    }
+}
